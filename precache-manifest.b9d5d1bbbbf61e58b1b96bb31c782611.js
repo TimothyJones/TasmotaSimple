a2a65ffd136d4b119deca7731cb2bf0e8a98310f
Timothy Jones <tjones@dius.com.au>
2019-12-21 17:00:55 +1300

    Updates

---
+++ precache-manifest.b9d5d1bbbbf61e58b1b96bb31c782611.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27e1e04e92c4a0f487338e0945f6772d",
+    "url": "/TasmotaSimple/index.html"
+  },
+  {
+    "revision": "53f4faea0d2e07e59acd",
+    "url": "/TasmotaSimple/static/css/main.d061d778.chunk.css"
+  },
+  {
+    "revision": "e650aa8b4320924d7355",
+    "url": "/TasmotaSimple/static/js/2.63a602a6.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/TasmotaSimple/static/js/2.63a602a6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "53f4faea0d2e07e59acd",
+    "url": "/TasmotaSimple/static/js/main.fd2afdd2.chunk.js"
+  },
+  {
+    "revision": "0f076ae5cf92667d4577",
+    "url": "/TasmotaSimple/static/js/runtime-main.e68d3e44.js"
+  }
+]);
