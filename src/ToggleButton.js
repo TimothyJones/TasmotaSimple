e9608833c85a362c4a3c7b80f25d84d98a5e50be
RadioNerd <richard.jones.1952@gmail.com>
2020-02-25 22:09:30 +1300

    changed updating message to offline

--- src/ToggleButton.js
+++ src/ToggleButton.js
@@ -67,7 +67,7 @@ const ToggleButton = ({ host, displayName }) => {
           device.toggle().then(wasItOn => setIsOn(wasItOn));
         }}
       >
-      {hasLoaded ? displayName : displayName + ':loading'}
+      {hasLoaded ? displayName : displayName + ':Offline'}
       </Button>
       <SettingsButton href={`http://${host}`}>
         <span role="img" aria-label="Gear">
