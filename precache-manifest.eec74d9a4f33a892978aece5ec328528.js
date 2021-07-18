ce47a55e648ce34f53b32899501ca325ce998478
Timothy Jones <timothy.jones@mebank.com.au>
2021-07-18 21:28:27 +1000

    Updates

---
+++ precache-manifest.eec74d9a4f33a892978aece5ec328528.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b504a48aac7c0cd55a329d4bcc2a4457",
+    "url": "/TasmotaSimple/index.html"
+  },
+  {
+    "revision": "cbc5690a4b479b356ed7",
+    "url": "/TasmotaSimple/static/css/main.d061d778.chunk.css"
+  },
+  {
+    "revision": "3bdf9a2dd8b293df6d16",
+    "url": "/TasmotaSimple/static/js/2.edb09e5d.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/TasmotaSimple/static/js/2.edb09e5d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cbc5690a4b479b356ed7",
+    "url": "/TasmotaSimple/static/js/main.d33a256f.chunk.js"
+  },
+  {
+    "revision": "0f076ae5cf92667d4577",
+    "url": "/TasmotaSimple/static/js/runtime-main.e68d3e44.js"
+  }
+]);
