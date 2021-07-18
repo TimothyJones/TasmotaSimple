a98e5e621939d68a33bd071043487d8404120405
Timothy Jones <timothy.jones@mebank.com.au>
2021-07-18 22:03:45 +1000

    fix: Add garage door, remove nodeMCU

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export const devices = [
   { displayName: 'Spare', host: 'spare.fritz.box' },
   { displayName: 'TV', host: 'tv.fritz.box' },
   { displayName: 'Lamp', host: 'lamp.fritz.box' },
-  { displayName: 'NodeMCU', host: 'nodemcu.fritz.box' },
+  { displayName: 'Garage Door', host: 'garage.fritz.box' },
   { displayName: 'Toilet towel rail', host: 'toilet.fritz.box' },
   { displayName: 'Shower towel rail', host: 'shower.fritz.box' },
   { displayName: 'Spa', host: 'spa.fritz.box' }
